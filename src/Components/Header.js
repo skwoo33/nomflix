3f9b98932f7c93ccc2a327d636e11b621ca696a0
SangKyu Woo <skwoo@yu.ac.kr>
2019-08-25 08:46:48 +0900

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link, withRouter } from 'react-router-dom';
+
+const Header = styled.header`
+  color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  background-color: rgba(20, 20, 20, 0.8);
+  /* background-color: red; */
+  z-index: 10;
+  box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
+`;
+
+const List = styled.ul`
+  display: flex;
+`;
+
+const Item = styled.li`
+  width: 80px;
+  height: 50px;
+  text-align: center;
+  border-bottom: 3px solid
+    ${props => (props.current ? '#3498db' : 'transparent')};
+  transition: border-bottom 0.5s ease-in-out;
+`;
+
+const SLink = styled(Link)`
+  height: 50px;
+  /* width: 100%;
+  height: 100%; */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default withRouter(({ location: { pathname } }) => (
+  <Header>
+    <List>
+      <Item current={pathname === '/'}>
+        <SLink to="/">Movies</SLink>
+      </Item>
+      <Item current={pathname === '/tv'}>
+        <SLink to="/tv">T V</SLink>
+      </Item>
+      <Item current={pathname === '/search'}>
+        <SLink to="/search">Search</SLink>
+      </Item>
+    </List>
+  </Header>
+));
